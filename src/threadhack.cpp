fb9bfa422010e6f260f54b001e4e463db22f76a5
kevin.moreirad <kevin.moreiradacosta@heig-vd.ch>
2017-03-06 15:44:30 +0100

    premiere modification

---
+++ src/threadhack.cpp
@@ -0,0 +1,6 @@
+#include "threadhack.h"
+
+ThreadHack::ThreadHack()
+{
+
+}
---
+++ src/threadhack.h
@@ -0,0 +1,11 @@
+#ifndef THREADHACK_H
+#define THREADHACK_H
+
+
+class ThreadHack :public QThread
+{
+public:
+    ThreadHack();
+};
+
+#endif // THREADHACK_H
